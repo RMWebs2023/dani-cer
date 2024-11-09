59565bab1bc003d9c8dcac8bb5f8b84712410af4
René <107271569+renevalderrey@users.noreply.github.com>
2024-11-09 19:58:47 -0300

    agregué un carrusel

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,38 +1,52 @@
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
+import { Link } from "react-router-dom";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Home = () => {
   return (
-    <main>
+    <>
       <Navbar />
-      <section className="home-container2">
-        <article>
-          <a href="/busquedas">
-            <img src="https://i.pinimg.com/736x/bf/95/34/bf953419d76bf747cba69b55e6e03957.jpg" />
-            <div className="overlay">
-              <p>Búsquedas laborales</p>
+      <div className="home-carousel-container">
+        <Carousel autoPlay infiniteLoop showThumbs={false} showStatus={false}>
+          <Link to="/contacto">
+            <div className="slide">
+              <img
+                src="https://sqt.com.ar/wp-content/uploads/2018/04/intro-hotel.jpg"
+                alt="Slide 1"
+              />
+              <div className="overlay">
+                <p className="text">Contacto</p>
+              </div>
             </div>
-          </a>
-        </article>
-        <article>
-          <a href="/busquedas">
-            <img src="https://i.pinimg.com/736x/bf/95/34/bf953419d76bf747cba69b55e6e03957.jpg" />
-            <div className="overlay">
-              <p>Búsquedas laborales</p>
+          </Link>
+          <Link to="/busquedas">
+            <div className="slide">
+              <img
+                src="https://sqt.com.ar/wp-content/uploads/2018/04/intro-home-busquedas2.jpg"
+                alt="Slide 2"
+              />
+              <div className="overlay">
+                <p className="text">Búsquedas Actuales</p>
+              </div>
             </div>
-          </a>
-        </article>
-        <article>
-          <a href="/busquedas">
-            <img src="https://i.pinimg.com/736x/bf/95/34/bf953419d76bf747cba69b55e6e03957.jpg" />
-            <div className="overlay">
-              <p>Búsquedas laborales</p>
+          </Link>
+          <Link to="quienes-somos">
+            <div className="slide">
+              <img
+                src="https://sqt.com.ar/wp-content/uploads/2018/04/intro-home-capacitacion.jpg"
+                alt="Slide 3"
+              />
+              <div className="overlay">
+                <p className="text">¿Quiénes somos?</p>
+              </div>
             </div>
-          </a>
-        </article>
-      </section>
+          </Link>
+        </Carousel>
+      </div>
       <Footer />
-    </main>
+    </>
   );
 };
 
