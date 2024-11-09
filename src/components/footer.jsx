d578842252923aaa3e9f910a4bd39d33c9fe5b1f
CortezWalter <cortezwaltera@gmail.com>
2024-11-09 20:20:11 -0300

    se corrige el class
    se corrige class por classname

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,44 +1,44 @@
 const Footer = () => {
     return(
-        <footer class="footer-distributed">
-			<div class="footer-left">
+        <footer className="footer-distributed">
+			<div className="footer-left">
 				<h3>Company<span>logo</span></h3>
-				<p class="footer-links">
-					<a href="#" class="link-1">Home</a>
+				<p className="footer-links">
+					<a href="#" className="link-1">Home</a>
 					<a href="#">Servicios</a>
 					<a href="#">Busquedas actuales</a>
 					<a href="#">Quienes somos</a>
 					<a href="#">Contact</a>
 				</p>
-				<p class="footer-company-name">RMWebs © 2024</p>
+				<p className="footer-company-name">RMWebs © 2024</p>
 			</div>
 
-			<div class="footer-center">
+			<div className="footer-center">
 				<div>
-					<i class="fa fa-map-marker"></i>
+					<i className="fa fa-map-marker"></i>
 					<p><span>Test 1234</span> Buenos Aires, Argentina</p>
 				</div>
 				<div>
-					<i class="fa fa-phone"></i>
+					<i className="fa fa-phone"></i>
 					<p>+54 9 123456789</p>
 				</div>
 				<div>
-					<i class="fa fa-envelope"></i>
+					<i className="fa fa-envelope"></i>
 					<p><a href="mailto:support@company.com">tesr@test.com</a></p>
 				</div>
 			</div>
 
-			<div class="footer-right">
-				<p class="footer-company-about">
+			<div className="footer-right">
+				<p className="footer-company-about">
 					<span>Sobre la empresa</span>
 					Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
 				</p>
                 
-				<div class="footer-icons">
-					<a href="#"><i class="fa fa-facebook"></i></a>
-					<a href="#"><i class="fa fa-twitter"></i></a>
-					<a href="#"><i class="fa fa-linkedin"></i></a>
-					<a href="#"><i class="fa fa-github"></i></a>
+				<div className="footer-icons">
+					<a href="#"><i className="fa fa-facebook"></i></a>
+					<a href="#"><i className="fa fa-twitter"></i></a>
+					<a href="#"><i className="fa fa-linkedin"></i></a>
+					<a href="#"><i className="fa fa-github"></i></a>
 				</div>
 			</div>
 		</footer>
