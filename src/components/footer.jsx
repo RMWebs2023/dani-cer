5f08dd13e739d84e732cd49c3e3de9414a17f807
CortezWalter <cortezwaltera@gmail.com>
2024-11-09 20:14:19 -0300

    Footer
    Footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,47 @@
 const Footer = () => {
     return(
-        <footer>
-            <>Este es el footer</>
-        </footer>
+        <footer class="footer-distributed">
+			<div class="footer-left">
+				<h3>Company<span>logo</span></h3>
+				<p class="footer-links">
+					<a href="#" class="link-1">Home</a>
+					<a href="#">Servicios</a>
+					<a href="#">Busquedas actuales</a>
+					<a href="#">Quienes somos</a>
+					<a href="#">Contact</a>
+				</p>
+				<p class="footer-company-name">RMWebs © 2024</p>
+			</div>
+
+			<div class="footer-center">
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>Test 1234</span> Buenos Aires, Argentina</p>
+				</div>
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>+54 9 123456789</p>
+				</div>
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:support@company.com">tesr@test.com</a></p>
+				</div>
+			</div>
+
+			<div class="footer-right">
+				<p class="footer-company-about">
+					<span>Sobre la empresa</span>
+					Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+				</p>
+                
+				<div class="footer-icons">
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					<a href="#"><i class="fa fa-linkedin"></i></a>
+					<a href="#"><i class="fa fa-github"></i></a>
+				</div>
+			</div>
+		</footer>
     )
 }
 
